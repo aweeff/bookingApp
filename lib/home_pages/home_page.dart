c36bfc3e3e64330ca587ccf998d5c405a5c76c8f
zero <maks6qw81@gmail.com>
2024-04-24 13:52:28 +0600

    commit crime

--- lib/home_pages/home_page.dart
+++ lib/home_pages/home_page.dart
@@ -1,7 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'my_tickets_page.dart';
+import 'my_favorites_page.dart';
+import 'profile_page.dart';
+import 'search_page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    MyTicketsPage(),
+    SearchPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,23 +34,30 @@ class HomePage extends StatelessWidget {
         title: const Text('Welcome'),
         leading: IconButton(
           icon: const Icon(Icons.close),
-          onPressed: () {
-          },
+          onPressed: () {},
         ),
-
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.bookmark_add_outlined),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyFavoritesPage()),
+              );
+            },
           ),
           IconButton(
             icon: const Icon(Icons.airplane_ticket_outlined),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyTicketsPage()),
+            );},
           ),
           PopupMenuButton<String>(
             onSelected: (String value) {},
             itemBuilder: (BuildContext context) {
-              return {'Option 1', 'Option 2', 'Option 3'}.map((String choice) {
+              return {'KAZ', 'RUS', 'ENG'}.map((String choice) {
                 return PopupMenuItem<String>(
                   value: choice,
                   child: Text(choice),
@@ -34,100 +66,118 @@ class HomePage extends StatelessWidget {
             },
           ),
         ],
-
-
       ),
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-
-
-            Container(
-              height: 200,
-              decoration: const BoxDecoration(
-                color: Colors.blue
-                /*image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/background_image.jpg'),
-                ),*/
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Departure',
+                hintText: 'Select your departure',
               ),
             ),
-
-
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Departure',
-                      hintText: 'Select your departure',
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Destination',
-                      hintText: 'Select your destination',
-                    ),
-
-                  ),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Departure Date',
-                      hintText: 'Select your departure date',
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Return Date',
-                      hintText: 'Select your return date',
-                    ),
-
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Passenger',
-                            hintText: 'Select passengers',
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Class',
-                            hintText: 'Select class',
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Search'),
-                    ),
-                  ),
-                  const Text(
-                    'Recent searches',
-                  ),
-                ],
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Destination',
+                hintText: 'Select your destination',
               ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Departure Date',
+                hintText: 'Select your departure date',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Return Date',
+                hintText: 'Select your return date',
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Passenger',
+                      hintText: 'Select passengers',
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Class',
+                      hintText: 'Select class',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add your search logic here
+                },
+                child: const Text('Search'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Recent searches',
             ),
           ],
         ),
       ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
+
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavigationBar({
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+      currentIndex: currentIndex,
+      selectedItemColor: Colors.blue,
+      onTap: onTap,
+    );
+  }
+}
+
